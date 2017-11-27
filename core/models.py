c28288488150719169eb6386fd45a66abb74b73c
Agnes Mutembei <mutembeiagnes@gmail.com>
2017-11-27 18:15:40 +0300

    added employee

--- core/models.py
+++ core/models.py
@@ -8,3 +8,28 @@ class Department(models.Model):
 
     def __str__(self):
         return self.department_id
+
+
+class Employee(models.Model):
+    STATUS = (
+        ('m', 'Married'),
+        ('s', 'Single'),
+        ('n', 'None')
+    )
+    EMP_STATUS = (
+        ('p', 'Permanent'),
+        ('c', 'Contractual')
+    )
+    employee_id = models.CharField(max_length=10, unique=True)
+    d_o_b = models.DateField(verbose_name='Date of Birth')
+    nationality = models.CharField(max_length=100)
+    employment = models.DateField(verbose_name='Date of Employment')
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, verbose_name='Mobile Phone')
+    status = models.CharField(max_length=1, choices=STATUS)
+    emp_status = models.CharField(verbose_name='Employment', max_length=1, choices=EMP_STATUS)
+
+    def __str__(self):
+        return self.employee_id
