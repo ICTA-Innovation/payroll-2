377c571d92bfa96f984c7caebad567b2c2484457
Agnes Mutembei <mutembeiagnes@gmail.com>
2017-11-27 18:53:15 +0300

    added employee work experience admin

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Department, Employee, EmployeeEducation
+from .models import Department, Employee, EmployeeEducation, EmployeeWorkExperience
 
 
 class DepartmentAdmin(admin.ModelAdmin):
@@ -16,8 +16,14 @@ class EmployeeEducationInline(admin.TabularInline):
     verbose_name_plural = 'Employee Education'
 
 
+class EmployeeWorkExperience(admin.TabularInline):
+    model = EmployeeWorkExperience
+    can_delete = True
+    extra = 1
+
+
 class EmployeeAdmin(admin.ModelAdmin):
-    inlines = [EmployeeEducationInline]
+    inlines = [EmployeeEducationInline, EmployeeWorkExperience]
     fields = ('employee_id', ('first_name', 'last_name'), ('email', 'phone'), ('d_o_b', 'nationality'),
               'status', ('employment', 'emp_status'))
     list_display = ('employee_id', 'first_name', 'last_name', 'email', 'phone', 'd_o_b',
